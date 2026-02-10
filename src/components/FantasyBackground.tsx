f0826533ed7bd141d096a4ae0309f4cc15a46620
Sucrit <smitholiver106@gmail.com>
2026-02-10 17:06:48 +0300

    .

--- src/components/FantasyBackground.tsx
+++ src/components/FantasyBackground.tsx
@@ -560,7 +560,7 @@ const FantasyBackground: React.FC = () => {
         width="100%"
         height="100%"
         viewBox="0 0 1920 1080"
-        preserveAspectRatio="xMidYMax slice"
+        preserveAspectRatio="xMidYMid slice"
         xmlns="http://www.w3.org/2000/svg"
         style={{ display: 'block' }}
       >
