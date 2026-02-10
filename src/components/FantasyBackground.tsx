1e146883a4ed1dffb92aebec8caceaf03aa5271f
Sucrit <smitholiver106@gmail.com>
2026-02-10 16:16:31 +0300

    mobile bg bug

--- src/components/FantasyBackground.tsx
+++ src/components/FantasyBackground.tsx
@@ -35,7 +35,9 @@ const FantasyBackground: React.FC = () => {
   useAnimationFrame((_t, delta) => {
     idleRef.current += delta * 0.003; 
     
-    const scrollOffset = scrollY.get() * 0.08; 
+    // On mobile, disable scroll-based parallax to prevent background movement
+    const isMobile = window.innerWidth <= 768;
+    const scrollOffset = isMobile ? 0 : scrollY.get() * 0.08; 
     const x = (idleRef.current + scrollOffset) % 1920;
     starX.set(x);
   });
